f0824152684222397011d31e23bdc804baa0a4d1
Ali <ali.m@bit24.info>
2023-09-14 22:41:47 +0330

    add force ssl

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -6,6 +6,11 @@ class DeployController extends Controller
 {
     public function index()
     {
+        $bashFile = file_get_contents(base_path('deploy.sh'));
+        $commands = explode(PHP_EOL, $bashFile);
+        foreach ($commands as $command) {
+            echo '<pre>'.shell_exec($command).'</pre>';
+        }
 
     }
 }
--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -16,6 +16,7 @@
 
     <div class="row">
         <div class="col-12 mb-2">
+            <h1>Deploy :|</h1>
             @include('admin.partial.message')
         </div>
         <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3">
