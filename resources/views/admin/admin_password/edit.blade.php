dab033c5c242aaf0202ecddd4674b45f1e29226c
ali <ali.mousavi@pomechain.com>
2023-11-01 21:39:23 +0330

    update admin module

--- resources/views/admin/admin_password/edit.blade.php
+++ resources/views/admin/admin_password/edit.blade.php
@@ -11,8 +11,10 @@
         <h1 class="page-title">{{ trans('panel.admin.title') }}</h1>
         <div>
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">{{ trans('panel.dashboard.title') }}</a></li>
-                <li class="breadcrumb-item"><a href="{{ route('admin.admin.index') }}">{{ trans('panel.admin.title') }}</a></li>
+                <li class="breadcrumb-item"><a
+                            href="{{ route('admin.dashboard') }}">{{ trans('panel.dashboard.title') }}</a></li>
+                <li class="breadcrumb-item"><a
+                            href="{{ route('admin.admin.index') }}">{{ trans('panel.admin.title') }}</a></li>
                 <li class="breadcrumb-item active">{{ trans('panel.admin.edit_password') }}</li>
             </ol>
         </div>
@@ -23,22 +25,75 @@
             <div class="card">
                 <div class="card-body pb-3">
                     @include('admin.partial.message')
-                    <form class="request-form forms-sample" enctype="multipart/form-data" method="post" action="{{ $routeUpdate }}">
+                    <form class="request-form forms-sample" enctype="multipart/form-data" method="post"
+                          action="{{ $routeUpdate }}">
                         @csrf
                         @method('PATCH')
 
+                        <table class="table table-hover mb-4">
+                            <tbody>
+                            <tr>
+                                <td>پست الکترونیک</td>
+                                <td><a href="mailto:{{ $admin->email }}">{{ $admin->email }}</a></td>
+
+                                <td>شماره همراه</td>
+                                <td><a href="tel:{{ $admin->mobile }}">{{ $admin->mobile }}</a></td>
+                            </tr>
+
+                            <tr>
+                                <td>شماره همراه شرکتی</td>
+                                <td>
+                                    @if($admin->mobile_company)
+                                        <a href="tel:{{ $admin->mobile_company }}">{{ $admin->mobile_company }}</a>
+                                    @endif
+                                </td>
+
+                                <td>شماره داخلی</td>
+                                <td>
+                                    @if($admin->number_company)
+                                        <a href="tel:{{ $admin->number_company }}">{{ $admin->number_company }}</a>
+                                    @endif
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>نام</td>
+                                <td>{{ $admin->first_name }}</td>
+
+                                <td>نام خانوادگی</td>
+                                <td>{{ $admin->last_name }}</td>
+                            </tr>
 
 
-                        <x-admin.input identify="password" :title="trans('fields.admin.password')" type="password" />
+                            <tr>
+                                <td>دسترسی</td>
+                                <td>@include('admin.partial.bool_badge',['value'=>$admin->has_access])</td>
+                                <td>سطح دسترسی</td>
+                                <td>{{ $admin->roles()->get()->implode('name',',') }}</td>
+                            </tr>
 
-                        <x-admin.input identify="password_rep" :title="trans('fields.admin.password_rep')" type="password" />
 
+                            <tr>
+                                <td>تاریخ ایجاد</td>
+                                <td>{{$admin->created_at->toJalali()->format('d F Y - H:i')}}</td>
+
+                                <td>آخرین ورود</td>
+                                <td>
+                                    @if($admin->latestLogin)
+                                        {{ $admin->latestLogin->login_at->toJalali()->format('d F Y - H:i') }}
+                                    @endif
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                        <x-admin.input identify="password" title="گذرواژه جدید" type="password"/>
+
+                        <x-admin.input identify="password_rep" title="تکرار گذرواژه" type="password"/>
 
                         <x-admin.button-submit title="{{ trans('panel.update') }}"/>
 
-
                     </form>
-
 
                 </div>
             </div>
