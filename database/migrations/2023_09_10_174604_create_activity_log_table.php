6a20f704c2dd93e076db4cfe49bcf202f03a7940
ali <ali.mousavi@pomechain.com>
2023-11-12 22:06:40 +0330

    update log activity table

--- database/migrations/2023_09_10_174604_create_activity_log_table.php
+++ database/migrations/2023_09_10_174604_create_activity_log_table.php
@@ -12,7 +12,10 @@ class CreateActivityLogTable extends Migration
             $table->bigIncrements('id');
             $table->string('log_name')->nullable();
             $table->text('description');
-            $table->nullableMorphs('subject', 'subject');
+
+            $table->string('subject_id')->nullable();
+            $table->string('subject_type')->nullable();
+
             $table->nullableMorphs('causer', 'causer');
             $table->json('properties')->nullable();
             $table->timestamps();
