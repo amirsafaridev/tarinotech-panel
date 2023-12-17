3bb4d58b758d8761f3e00172be1adc6c30380a0f
ali <ali.mousavi@pomechain.com>
2023-12-17 18:19:48 +0330

    fix redirect

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,12 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (App::isProduction()) {
-            URL::forceRootUrl('https://portal.tarinotech.com');
             URL::forceScheme('https');
-
-            $_SERVER['SERVER_NAME'] = 'portal.tarinotech.com';
-            $_SERVER['HTTP_HOST'] = 'portal.tarinotech.com';
-
         }
         Paginator::useBootstrapFour();
     }
