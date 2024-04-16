fe1c9cab73f29e3539a36306ae2b321216011247
ali <ali.mousavi@pomechain.com>
2024-04-16 13:59:22 +0330

    remove auto auth

--- Modules/Auth/app/Http/Controllers/Admin/LoginController.php
+++ Modules/Auth/app/Http/Controllers/Admin/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
 
     public function index()
     {
-        Auth::guard('admin')->loginUsingId(1);
         $title = self::INDEX_TITLE;
 
         if (app()->isLocal()) {
