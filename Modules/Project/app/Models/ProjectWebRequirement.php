6852fe89394aeb1d85713bd6a7cd500cf708a486
ali <ali.mousavi@pomechain.com>
2024-10-23 19:14:46 +0330

    remove file_name from signable_files

---
+++ Modules/Project/app/Models/ProjectWebRequirement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Project\app\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectWebRequirement extends Model
+{
+    use HasFactory;
+}
---
+++ Modules/Project/database/factories/ProjectWebRequirementFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Project\database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\Project\app\Models\ProjectWebRequirement>
+ */
+class ProjectWebRequirementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
