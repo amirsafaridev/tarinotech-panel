df2c8fb4ef41da7a05d2f9c703db9f94d19a1707
Ali Mousavi <alimousavi@Alis-MacBook-Air.local>
2024-12-04 11:53:56 +0330

    update

--- Modules/Contract/resources/views/admin/signable/index.blade.php
+++ Modules/Contract/resources/views/admin/signable/index.blade.php
@@ -74,7 +74,7 @@
                                         <td>{{ verta($signable->created_at)->format(formatJalaliDateTime()) }}</td>
                                         <td class="d-flex gap-2">
                                             <a class="btn btn-sm btn-info" target="_blank" href="{{ makeRouteContractPreview($signable->target_type, $signable->target_id) }}">{{ __('panel.action.printContract') }}</a>
-                                            <a class="btn btn-sm btn-edit" target="_blank" href="{{ route('admin.contract.sign.edit', $signable->id) }}">{{ __('panel.action.edit') }}</a>
+                                            <a class="btn btn-sm btn-warning" target="_blank" href="{{ route('admin.contract.sign.edit', $signable->id) }}">{{ __('panel.action.edit') }}</a>
                                         </td>
                                     </tr>
                                 @endforeach
