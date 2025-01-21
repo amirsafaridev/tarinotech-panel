1708d2ca6ab106e5d5808ccbedb77c37a2e8ad3d
alimousavi71 <mosaviali701@gmail.com>
2025-01-21 22:43:35 +0330

    chore(history): change header title to better reflect content in history.blade.php

--- Modules/Package/resources/views/admin/part/history.blade.php
+++ Modules/Package/resources/views/admin/part/history.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
-        <h3 class="card-title">تاریخچه تغییرات</h3>
+        <h3 class="card-title">آرشیو متن قرارداد</h3>
     </div>
     <div class="card-body">
         <div class="table-responsive">
