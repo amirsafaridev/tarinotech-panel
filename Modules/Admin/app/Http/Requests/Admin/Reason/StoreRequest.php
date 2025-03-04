4bf202de13bd5af90054c3d52373b65bb27a40c3
AMIRHOSSAIN Karimi <amirhossainkarimi80@gmail.com>
2025-03-04 19:58:57 +0330

    Developping

---
+++ Modules/Admin/app/Http/Requests/Admin/Reason/StoreRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Admin\app\Http\Requests\Admin\Reason;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'description' => 'required',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
